c5eea6c84fd07dc8a6486c1c82b1da15ead8073e
Ilya Lisin <blacklacost@gmail.com>
2022-07-21 20:21:25 +0300

    Server for react

---
+++ server.mjs
@@ -0,0 +1,17 @@
+import express from 'express'
+import path from 'path'
+
+const app = express()
+const port = process.env.PORT || 4000
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+  })
+}
+
+app.listen(port, (err) => {
+  if (err) return console.log(err)
+  console.log('Server running on port: ', port)
+})
